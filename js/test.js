ecea4f3d4cdece7f6f8c32739b7a2edf61691209
sun-yun-long <1621563513@qq.com>
2020-09-11 11:09:21 +0800

    更改了样式

--- js/test.js
+++ js/test.js
@@ -4,6 +4,6 @@ $('p').on('click',function(){
     var a=Math.round(Math.random()*255);
     var b=Math.round(Math.random()*255);
     var c=Math.round(Math.random()*255);
-    $(this).css('color','rgb('+a+','+b+','+c+')');
+    $(this).css('background-color','rgb('+a+','+b+','+c+')');
     
 });
